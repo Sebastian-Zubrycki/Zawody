038ac2df90346317e9580cd7163c0a36106a3030
Sebastian <sebastianzubrycki@gmail.com>
2023-08-23 12:20:45 +0200

    Stworzenie szablonu aplikacji

---
+++ src/test/java/com/example/zawody/ZawodyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zawody;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZawodyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
